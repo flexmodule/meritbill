707fa7e5cad41ea95310dcb99afae220e142ee24
zhuohai <1181316781@qq.com>
2018-10-24 18:22:42 +0800

    新增

--- src/utils/env.js
+++ src/utils/env.js
@@ -13,7 +13,7 @@ let authUrl;
 let baseUrl2;
 const imgBaseUrl = 'https://fuss10.elemecdn.com';
 let regOpen;
-baseUrl = 'http://localhost:3000';
+baseUrl = 'http://139.199.201.128';
 routerMode = 'hash'
 authUrl = 'http://uat.sssyin.cn/tour/static/tour.html';
 regOpen = true;
